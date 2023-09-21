13911362ea28b77094e809b8453025f83cea13dd
YenJuiChen <122350369+YenJuiChen@users.noreply.github.com>
2023-09-21 17:33:55 +0800

    Update app_v3.py

--- app_v3.py
+++ app_v3.py
@@ -1,1 +1,118 @@
+import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
+#from BrowserOp import transcodeWebPage  # 假設您已經安裝了這個模塊或者使用了相應的API
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=500,
+        chunk_overlap=100,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    #llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.8,"max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+
+def handle_userinput(user_question):
+    if st.session_state.conversation is None:
+        st.write("對話系統還未初始化。請先上傳PDF文件並點擊 '運行'。")
+        return
+
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    # 檢查response是否包含有效答案
+    if not response['chat_history']:  # 假設沒有答案時chat_history是空的
+        # 進行網路搜尋
+        search_results = BrowserOp.transcodeWebPage(link=f"https://www.google.com/search?q={user_question}")
+        # 將搜尋結果顯示給用戶或重新提問
+        st.write(f"對於您的問題，我在網上找到了以下資訊：{search_results}")
+        return
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="YenJuiChen_PDF專家系統_v3",
+                       page_icon="\u2600")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []  # Initialize chat_history
+
+    st.header("YenJuiChen_PDF專家系統_v3 \u2600")
+    user_question = st.text_input("(Step 3)輸入問題:")
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("你的檔案")
+        pdf_docs = st.file_uploader(
+            "(Step 1)上傳檔案並點擊 '運行'", accept_multiple_files=True)
+        if st.button("(Step 2)運行"):
+            with st.spinner("運行中"):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(
+                    vectorstore)
+
+
+if __name__ == '__main__':
+    main()
 
